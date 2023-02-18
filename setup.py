50a894bed78670273ae35aa05a1d710f4d31f876
Rajiv <rajivgg91@gmail.com>
2023-02-17 21:54:44 -0600

    stable

--- setup.py
+++ setup.py
@@ -4,18 +4,18 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='toolbox',
-    version='0.0.3',
-    author='Mike Huls',
-    author_email='mike_huls@hotmail.com',
-    description='Testing installation of Package',
+    name='poseComm',
+    version='0.0.1',
+    author='Rajiv Gonzalez',
+    author_email='rajiv@tryiris.ai',
+    description='Package for the ipc communication between the pose estimator and the autotracking',
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url='https://github.com/mike-huls/toolbox',
+    url='https://github.com/tryiris-ai/Pose.estimator.comm.git',
     project_urls = {
-        "Bug Tracker": "https://github.com/mike-huls/toolbox/issues"
+        "Bug Tracker": "https://github.com/tryiris-ai/Pose.estimator.comm/issues"
     },
     license='MIT',
-    packages=['toolbox'],
-    install_requires=['requests'],
+    packages=['PoseEstimatorComm'],
+    install_requires=['aio-udp-server'],
 )
